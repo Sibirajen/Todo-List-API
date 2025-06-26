cdefc394646ddab9f0c9f8fac3edc05df636e4c7
Sibirajen <Sibirajen16@gmail.com>
2025-06-26 12:11:03 +0530

    Added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- Insert Users
+INSERT INTO Users (ID, NAME, EMAIL, PASSWORD) VALUES
+(1, 'Sibirajen', 'sibirajen16@gmail.com', '$2a$12$Hl6rZJwqbi0pgDuFhNq8TeocGPQf8tKsak9LtYxJX.h/yCYtYrada');
+
+-- Insert Tasks
+INSERT INTO TASKS (ID, TITLE, DESCRIPTION) VALUES
+(1, 'Learn Spring Boot', 'Go through CRUD operations using Spring Boot'),
+(2, 'Finish React Frontend', 'Build the React UI for the Todo list app'),
+(3, 'Prepare Resume', 'Update resume with latest project and skills'),
+(4, 'Read Clean Code', 'Start reading Clean Code by Robert C. Martin');
