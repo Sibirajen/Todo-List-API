8ce6bec2d95b6c369e27db96382d622e3e4243d0
Sibirajen <Sibirajen16@gmail.com>
2025-06-26 11:57:47 +0530

    Added User Entity

---
+++ src/main/java/com/sibirajen/Todo/List/API/model/User.java
@@ -0,0 +1,23 @@
+package com.sibirajen.Todo.List.API.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Users")
+@Access(AccessType.FIELD)
+public class User extends BaseEntity{
+    @Column(name = "NAME", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "EMAIL", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+}
