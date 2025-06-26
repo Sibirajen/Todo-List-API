1ad1252d7ed1d76ea3fd1da5bbba96a9e0d528d8
Sibirajen <Sibirajen16@gmail.com>
2025-06-26 11:57:16 +0530

    Added base entity with ID

---
+++ src/main/java/com/sibirajen/Todo/List/API/model/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.sibirajen.Todo.List.API.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+@Access(AccessType.FIELD)
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", updatable = false, unique = true, nullable = false)
+    private Long id;
+}
