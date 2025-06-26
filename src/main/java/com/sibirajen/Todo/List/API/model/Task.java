35badbc65fb5d337baacee95787db18e0bba5180
Sibirajen <Sibirajen16@gmail.com>
2025-06-26 11:57:37 +0530

    Added Task Entity

---
+++ src/main/java/com/sibirajen/Todo/List/API/model/Task.java
@@ -0,0 +1,20 @@
+package com.sibirajen.Todo.List.API.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "TASKS")
+@Access(AccessType.FIELD)
+public class Task extends BaseEntity{
+    @Column(name = "TITLE", nullable = false)
+    private String title;
+
+    @Column(name = "DESCRIPTION", length = 1000)
+    private String description;
+}
