1e057a83d502ad583b7e5ad4c74c9c3384aa5f8f
Sibirajen <Sibirajen16@gmail.com>
2025-06-27 13:08:24 +0530

    Added token dto

---
+++ src/main/java/com/sibirajen/Todo/List/API/dto/Token.java
@@ -0,0 +1,12 @@
+package com.sibirajen.Todo.List.API.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Token {
+    private String token;
+}
