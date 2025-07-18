2c0b9659335886274753a49489aabdaf409640da
Sibirajen <Sibirajen16@gmail.com>
2025-07-18 18:15:45 +0530

    made them lowercase

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 -- Insert Users
-INSERT INTO Users (ID, NAME, EMAIL, PASSWORD) VALUES
-(1, 'Sibirajen', 'sibirajen16@gmail.com', '$2a$12$Hl6rZJwqbi0pgDuFhNq8TeocGPQf8tKsak9LtYxJX.h/yCYtYrada');
+INSERT INTO user (id, name, email, password, role) VALUES
+(1, 'Sibirajen', 'sibirajen16@gmail.com', '$2a$12$Hl6rZJwqbi0pgDuFhNq8TeocGPQf8tKsak9LtYxJX.h/yCYtYrada', 'user');
 
 -- Insert Tasks
-INSERT INTO TASKS (ID, TITLE, DESCRIPTION) VALUES
+INSERT INTO tasks (id, title, description) VALUES
 (1, 'Learn Spring Boot', 'Go through CRUD operations using Spring Boot'),
 (2, 'Finish React Frontend', 'Build the React UI for the Todo list app'),
 (3, 'Prepare Resume', 'Update resume with latest project and skills'),
