f12eec727171bcb98b25d644cc397059fd41f0f9
Sibirajen <Sibirajen16@gmail.com>
2025-07-18 18:14:32 +0530

    implemented UserDetails Interface

--- src/main/java/com/sibirajen/Todo/List/API/model/User.java
+++ src/main/java/com/sibirajen/Todo/List/API/model/User.java
@@ -1,24 +1,70 @@
 package com.sibirajen.Todo.List.API.model;
 
+import com.sibirajen.Todo.List.API.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString(callSuper = true)
+@ToString(callSuper = true, exclude = "password")
 @Entity
-@Table(name = "Users")
+@Table(name = "user")
 @Access(AccessType.FIELD)
-public class User extends BaseEntity{
-    @Column(name = "NAME", nullable = false, length = 100)
+public class User extends BaseEntity implements UserDetails {
+    @Column(name = "name", nullable = false, length = 100)
     private String name;
 
-    @Column(name = "EMAIL", nullable = false, unique = true)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "PASSWORD", nullable = false)
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
