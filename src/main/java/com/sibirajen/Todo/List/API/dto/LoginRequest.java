172a65eb029ca54c9de9b39ebc748aa49e51dbc7
Sibirajen <Sibirajen16@gmail.com>
2025-07-19 18:44:46 +0530

    Added proper validations

--- src/main/java/com/sibirajen/Todo/List/API/dto/LoginRequest.java
+++ src/main/java/com/sibirajen/Todo/List/API/dto/LoginRequest.java
@@ -1,5 +1,7 @@
 package com.sibirajen.Todo.List.API.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -9,6 +11,10 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 }
--- src/main/java/com/sibirajen/Todo/List/API/dto/RegisterRequest.java
+++ src/main/java/com/sibirajen/Todo/List/API/dto/RegisterRequest.java
@@ -1,6 +1,12 @@
 package com.sibirajen.Todo.List.API.dto;
 
+import com.sibirajen.Todo.List.API.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.apache.catalina.CredentialHandler;
 
 @Getter
 @Setter
@@ -9,7 +15,19 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "Name is required")
+    @Size(max = 50, message = "Name must not exceed 50 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    @Size(max = 100, message = "Email must not exceed 100 characters")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
     private String password;
+
+    @NotNull(message = "Role must be specified")
+    private Role role;
 }
