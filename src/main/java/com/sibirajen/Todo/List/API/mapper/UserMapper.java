a6b8a28a347ac966de440e2022161eebf161b35d
Sibirajen <Sibirajen16@gmail.com>
2025-07-19 18:46:32 +0530

    Encoded password

--- src/main/java/com/sibirajen/Todo/List/API/mapper/UserMapper.java
+++ src/main/java/com/sibirajen/Todo/List/API/mapper/UserMapper.java
@@ -2,13 +2,15 @@ package com.sibirajen.Todo.List.API.mapper;
 
 import com.sibirajen.Todo.List.API.dto.RegisterRequest;
 import com.sibirajen.Todo.List.API.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class UserMapper {
-    public static User toEntity(RegisterRequest request){
+    public static User toEntity(RegisterRequest request, PasswordEncoder passwordEncoder){
         return User.builder()
                 .name(request.getName())
-                .email(request.getEmail())
-                .password(request.getPassword())
+                .email(request.getEmail().toLowerCase())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
                 .build();
     }
 }
