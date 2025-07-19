df90c6ccbd44026869b0cac039dd9df150797ec4
Sibirajen <Sibirajen16@gmail.com>
2025-07-19 18:46:09 +0530

    Added proper validations

--- src/main/java/com/sibirajen/Todo/List/API/dto/TaskRequest.java
+++ src/main/java/com/sibirajen/Todo/List/API/dto/TaskRequest.java
@@ -1,5 +1,7 @@
 package com.sibirajen.Todo.List.API.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,6 +11,11 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TaskRequest {
+    @NotBlank(message = "Title cannot be blank")
+    @Size(max = 100, message = "Title can be at most 100 characters")
     private String title;
+
+    @NotBlank(message = "Description cannot be blank")
+    @Size(max = 1000, message = "Description can be at most 1000 characters")
     private String description;
 }
