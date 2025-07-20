0a785cdf4240eb269b7f7cc17eeb2f89ca0f0882
Sibirajen <Sibirajen16@gmail.com>
2025-07-20 19:49:38 +0530

    Made to auto generate id

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 -- Insert Users
-INSERT INTO users (id, name, email, password, role) VALUES
-(1, 'Sibirajen', 'sibirajen16@gmail.com', '$2a$12$Hl6rZJwqbi0pgDuFhNq8TeocGPQf8tKsak9LtYxJX.h/yCYtYrada', 'user');
+INSERT INTO users (name, email, password, role) VALUES
+('Sibirajen', 'sibirajen16@gmail.com', '$2a$12$Hl6rZJwqbi0pgDuFhNq8TeocGPQf8tKsak9LtYxJX.h/yCYtYrada', 'user');
 
 -- Insert Tasks
-INSERT INTO tasks (id, title, description) VALUES
-(1, 'Learn Spring Boot', 'Go through CRUD operations using Spring Boot'),
-(2, 'Finish React Frontend', 'Build the React UI for the Todo list app'),
-(3, 'Prepare Resume', 'Update resume with latest project and skills'),
-(4, 'Read Clean Code', 'Start reading Clean Code by Robert C. Martin');
+INSERT INTO tasks (title, description) VALUES
+('Learn Spring Boot', 'Go through CRUD operations using Spring Boot'),
+('Finish React Frontend', 'Build the React UI for the Todo list app'),
+('Prepare Resume', 'Update resume with latest project and skills'),
+('Read Clean Code', 'Start reading Clean Code by Robert C. Martin');
