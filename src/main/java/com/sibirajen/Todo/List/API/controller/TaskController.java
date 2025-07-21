3df0cc53ff304bbe288768fef0542c2a4a2e306b
Sibirajen <Sibirajen16@gmail.com>
2025-07-21 16:50:39 +0530

    Added create, update and delete task endpoint

---
+++ src/main/java/com/sibirajen/Todo/List/API/controller/TaskController.java
@@ -0,0 +1,42 @@
+package com.sibirajen.Todo.List.API.controller;
+
+import com.sibirajen.Todo.List.API.dto.TaskRequest;
+import com.sibirajen.Todo.List.API.dto.TaskResponse;
+import com.sibirajen.Todo.List.API.service.TaskService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class TaskController {
+    private final TaskService taskService;
+
+    @PostMapping("/todos")
+    public ResponseEntity<TaskResponse> createTask(
+            @Valid @RequestBody TaskRequest taskRequest
+    ){
+        TaskResponse taskResponse = this.taskService.createTask(taskRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskResponse);
+    }
+
+    @PostMapping("/todos/{id}")
+    public ResponseEntity<TaskResponse> updateTask(
+            @PathVariable Long id,
+            @Valid @RequestBody TaskRequest taskRequest
+    ){
+        TaskResponse taskResponse = this.taskService.updateTask(id, taskRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskResponse);
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public ResponseEntity<Void> deleteTask(
+            @PathVariable Long id
+    ){
+        this.taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+}
